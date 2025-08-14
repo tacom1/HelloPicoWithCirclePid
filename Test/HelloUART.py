adfb8322de4412c5949e5a0b481406c8619106ea
tacom <1017208039@qq.com>
2025-08-14 20:36:25 +0800

    进度提交，开始尝试使用定时器接受byte

--- Test/HelloUART.py
+++ Test/HelloUART.py
@@ -1,4 +1,4 @@
-from machine import UART, Pin
+from machine import UART, Pin, Timer
 import time
 
 
@@ -12,33 +12,62 @@ only defined commands can be deeled.
 Next command will supports:
 
 Self Define Read Command(3-bytes):
-    1. M00: Connect / ACK
+    1. M00: Connect(Ask is Ok) / ACK
     2. M01: Read Encode / DATA
     3. M02: Read Left / DATA
     4. M03: Read Right / DATA
 
 Self Define Write Command:
-    1. G01: (3-bytes) | Data(half-float, 15bytes)
+    1. G01: (3-bytes) | Data(5-bytes)  # G01 18.8
 
 Self Define FallBack:
-    -  ACK:  0x01
-    -  DATA: 0x02 | Data(half-float, 15-bytes) 
+    -  ACK:  0x01 0x0A(\n)
+    -  DATA: 0x02 | Data 0x0A(\n)
 
 """
-def uart_rx(uart):
-    while uart.any():
-        print(uart.read(1))
 
 uart0 = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
+rx_buffer_command = bytearray()
+rx_buffer_data = bytearray()
+rx_tim = Timer()
 
-rx = bytearray()
-while True:
+def clean_buffer(clean_command, clean_data):
+    global rx_buffer_command
+    global rx_buffer_data
+    if clean_command:
+        rx_buffer_command[:] = b''
+    if clean_data:
+        rx_buffer_data[:] = b''
+
+def read_uart_freq(timer):
+    global uart0
+    global rx_buffer_command
+    global rx_buffer_data
+    
     # read
     if uart0.any() > 0:
-        rx.append(uart0.read(3))
-        s_rx = bytes(rx).decode('utf-8')
+        b = uart0.read(1)
+        if len(rx_buffer_command) < 3:
+            if len(rx_buffer_command) == 0:
+                if b != b'M' and b != b'G':
+                    return # Dirty Data Drop
+            rx_buffer_command.extend(b)
+        elif bytes(rx_buffer_command) == b'G01':
+            if len(rx_buffer_data) < 4 and b != b' ':  # blank key
+                rx_buffer_data.extend(b)
+            #else:
+                # else drop, until command be processed
+                # print("Drop", b)
+    
+    ### simulate main loop process data ###
+    if len(rx_buffer_command) == 3:
+        if bytes(rx_buffer_command) == b'M00':
+            uart0.write(bytes([0x01, 0x0A]))
         
-    else:
-        time.sleep(0.1)
+
+rx_tim.init(freq=20, mode=Timer.PERIODIC, callback=read_uart_freq)
+
+while True:
+    time.sleep(0.1)
         
         
